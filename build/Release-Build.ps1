53d4d4f74724c5f1a0f60dbff0a19bbdb85d9bd3
Mirko Maggioni <mirko.maggioni@gmail.com>
2017-07-24 15:37:40 +0200

    nuget.exe is added

--- build/Release-Build.ps1
+++ build/Release-Build.ps1
@@ -29,11 +29,26 @@ function SetAssemblyInfo([string]$sourceDirectory, [string]$version) {
 
 $msbuild = "C:\Program Files (x86)\MSBuild\14.0\bin\amd64\msbuild.exe"
 $solutionFolder = $PSScriptRoot + "..\..\"
+$solutionFile = $solutionFolder + "\EFContextMock.sln"
 $projectFile = $solutionFolder + "\DALTest\DAL.Test.csproj"
+$nuget = $solutionFolder + "\nuget\nuget.exe"
 $options = "/p:Configuration=Release"
 $version = $(git describe --abbrev=0 --tag)
 
 SetAssemblyInfo $solutionFolder $version
+
+Write-Host "Restore packages"
+
+& $nuget restore $solutionFile
+
+if ($LastExitCode -ne 0){
+    $exitCode=$LastExitCode
+    Write-Error "Build failed!"
+    exit $exitCode
+}
+else{
+    Write-Host "Build succeeded"
+}
 
 Write-Host "Building"
 
