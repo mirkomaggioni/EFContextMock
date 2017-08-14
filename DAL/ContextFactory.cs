6642e73b020be256f3ad4f1d3971bc4ace4acc17
Mirko Maggioni <mirko.maggioni@sintel.net>
2017-08-14 11:19:41 +0200

    context type checking

--- DAL/ContextFactory.cs
+++ DAL/ContextFactory.cs
@@ -5,16 +5,18 @@ namespace DAL
 {
 	public class ContextFactory
 	{
+		private Type _dbContextType;
 		private DbContext _dbContext;
 
 		public void Register<TDbContext>(TDbContext dbContext) where TDbContext : DbContext, new()
 		{
+			_dbContextType = typeof(TDbContext);
 			_dbContext = dbContext;
 		}
 
 		public TDbContext Get<TDbContext>() where TDbContext : DbContext, new()
 		{
-			if (_dbContext == null)
+			if (_dbContext == null || _dbContextType != typeof(TDbContext))
 			{
 				return new TDbContext();
 			}
