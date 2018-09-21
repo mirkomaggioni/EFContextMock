133416c072bf80c4b6351ef7f39e8998f3cee774
Mirko Maggioni <mirko.maggioni@gmail.com>
2018-09-21 19:33:56 +0200

    logger service is implemented

---
+++ DAL/BusinessLayer/LoggerService.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using DAL.Interfaces;
+
+namespace DAL.BusinessLayer
+{
+	public class LoggerService : ILoggerService
+	{
+		private readonly string _filePath;
+
+		public LoggerService(string filepath)
+		{
+			_filePath = filepath;
+		}
+
+		public void Debug(string message)
+		{
+			using (var streamWriter = new StreamWriter(_filePath, true))
+			{
+				streamWriter.WriteLine(message);
+			}
+		}
+	}
+}
---
+++ DAL/Interfaces/ILoggerService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+	public interface ILoggerService
+	{
+		void Debug(string message);
+	}
+}
